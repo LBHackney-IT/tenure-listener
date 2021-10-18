cf4a95092cace1f011ddad8a934311adb4518694
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-10-18 16:19:51 +0100

    Feature/mttl 1741 #4 (#40)
    * Update account tenure type
    
    * Update config.yml

--- TenureListener/Domain/Account/AccountTenure.cs
+++ TenureListener/Domain/Account/AccountTenure.cs
@@ -5,7 +5,7 @@ namespace TenureListener.Domain.Account
     public class AccountTenure
     {
         public string TenureId { get; set; }
-        public string TenureType { get; set; }
+        public AccountTenureType TenureType { get; set; }
         public List<AccountTenant> PrimaryTenants { get; set; }
         public string FullAddress { get; set; }
     }
